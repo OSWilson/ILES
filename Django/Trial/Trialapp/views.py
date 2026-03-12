b2475c428909a567b03fd5af46ae2fd3dda797b0
Oswilson <stewartoluka@gmail.com>
2026-03-12 22:55:16 +0300

    Remove unnecessary files

--- Django/Trial/Trialapp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-#from django.http import HttpResponse
-
-# Create your views here.
-def sample(request):
-    #return HttpResponse('<h1>THIS IS A SAMPLE PAGE</h1>')
-    return render(request, 'trialapp/index.html')
