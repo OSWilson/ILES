0a0426dc8f3ba84de95b48533f45af21965f2501
SsimbwaRicky <rickyssimbwa71@gmail.com>
2026-04-25 00:31:25 +0300

    added serializer for weekly log

--- backend/iles/serializers.py
+++ backend/iles/serializers.py
@@ -14,3 +14,29 @@ class CustomTokenSerializer(TokenObtainPairSerializer):
             'role': self.user.role,
         }
         return data
+        
+        
+        
+
+
+class WeeklyLogSerializer(serializers.ModelSerializer):
+    student_name = serializers.CharField(source='placement.student.full_name', read_only=True)
+    company_name = serializers.CharField(source='placement.company_name', read_only=True)
+
+    class Meta:
+        model = WeeklyLog
+        fields = [
+            'id', 
+            'placement', 
+            'student_name',
+            'company_name',
+            'week_number', 
+            'start_date', 
+            'end_date', 
+            'log_content', 
+            'status', 
+            'created_at', 
+            'updated_at',
+            'submitted_at'
+        ]
+        read_only_fields = ['status', 'submitted_at', 'created_at', 'updated_at']
