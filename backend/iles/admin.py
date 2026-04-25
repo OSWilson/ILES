8030aad28d67a386fc775cf6b0aec2378bcaa932
P3N012 <p3n012@gmail.com>
2026-04-25 16:44:30 +0300

    Add EvaluationCriteria and CriteriaScore to admin
    Registered EvaluationCriteria and CriteriaScore models in admin.

--- backend/iles/admin.py
+++ backend/iles/admin.py
@@ -1,19 +1,26 @@
 # Register your models here.
-
 from django.contrib import admin
-from .models import CustomUser, InternshipPlacement, WeeklyLog
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, InternshipPlacement, WeeklyLog, EvaluationCriteria, Evaluation, CriteriaScore
 
-
-
+admin.site.register(EvaluationCriteria)
+admin.site.register(CriteriaScore)
 
 @admin.register(WeeklyLog)
 class WeeklyLogAdmin(admin.ModelAdmin):
-   
     list_display = ('week_number', 'get_student', 'status', 'start_date', 'end_date', 'submitted_at')    
     list_filter = ('status', 'week_number')       
     search_fields = ('placement__student__username', 'placement__student__first_name', 'log_content')
 
-
     def get_student(self, obj):
         return obj.placement.student.full_name
     get_student.short_description = 'Student'
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'role', 'is_active')
+    list_filter = ('role',)
+    fieldsets = UserAdmin.fieldsets + (('ILES', {'fields': ('role', 'department', 'staff_number', 'student_number')}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (('ILES', {'fields': ('role', 'department', 'staff_number', 'student_number')})
+
+
