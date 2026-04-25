e46edfa17ded9b19a13c7d932d3f9a732e379c70
OSWilson <stewartoluka@gmail.com>
2026-04-25 09:21:33 +0300

    Added LoginView

--- backend/iles/views.py
+++ backend/iles/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .models import CustomUser, InternshipPlacement, WeeklyLog, EvaluationCriteria, Evaluation, CriteriaScore
+from .serializers import (
+    CustomTokenSerializer, RegisterSerializer, UserSerializer,
+    PlacementSerializer, WeeklyLogSerializer,
+    CriteriaSerializer, CriteriaScoreSerializer, EvaluationSerializer
+)
 
-# Create your views here.
+
+
+
+class LoginView(TokenObtainPairView):
+    serializer_class = CustomTokenSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
