18c5d969b1ded898ff5d5dc14be9a289a323328d
OSWilson <stewartoluka@gmail.com>
2026-04-27 10:22:47 +0300

    Added api/client.js

---
+++ frontend/src/api/client.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+
+const client = axios.create({ baseURL: 'http://localhost:8000/api' })
+
+client.interceptors.request.use(config => {
+  const token = localStorage.getItem('access_token')
+  if (token) config.headers.Authorization = `Bearer ${token}`
+  return config
+})
+
+client.interceptors.response.use(
+  res => res,
+  async err => {
+    const original = err.config
+    if (err.response?.status === 401 && !original._retry) {
+      original._retry = true
+      try {
+        const { data } = await axios.post('http://localhost:8000/api/auth/refresh/', {
+          refresh: localStorage.getItem('refresh_token')
+        })
+        localStorage.setItem('access_token', data.access)
+        original.headers.Authorization = `Bearer ${data.access}`
+        return client(original)
+      } catch {
+        localStorage.clear()
+        window.location.href = '/login'
+      }
+    }
+    return Promise.reject(err)
+  }
+)
+
+export default client
