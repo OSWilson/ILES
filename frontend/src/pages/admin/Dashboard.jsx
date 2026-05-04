83ff2a0f2595cb104fc6a129bb1cb33334670e80
Eric-zard <wanderaerickson01@gmail.com>
2026-05-03 20:07:25 -0800

    Add initial Dashboard component structure

---
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -0,0 +1,5 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { useState } from 'react'
+import Layout from '../../components/Layout'
+import client from '../../api/client'
+import styles from './Dashboard.module.css'
