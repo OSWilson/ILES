8d02ef68aee9758d8326ce1e293e0b794e8b73f6
OSWilson <stewartoluka@gmail.com>
2026-06-09 13:36:55 +0300

    Error fixing

--- backend/iles/admin.py
+++ backend/iles/admin.py
@@ -1,32 +1,35 @@
-# Register your models here.
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, InternshipPlacement, WeeklyLog, EvaluationCriteria, Evaluation, CriteriaScore
 
-@admin.register(WeeklyLog)
-class WeeklyLogAdmin(admin.ModelAdmin):
-    list_display = ('week_number', 'get_student', 'status', 'start_date', 'end_date', 'submitted_at')    
-    list_filter = ('status', 'week_number')       
-    search_fields = ('placement__student__username', 'placement__student__first_name', 'log_content')
-
-    def get_student(self, obj):
-        return obj.placement.student.full_name
-    get_student.short_description = 'Student'
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'email', 'role', 'is_active')
     list_filter = ('role',)
-    fieldsets = UserAdmin.fieldsets + (('ILES', {'fields': ('role', 'department', 'staff_number', 'student_number')}),)
-    add_fieldsets = UserAdmin.add_fieldsets + (('ILES', {'fields': ('role', 'department', 'staff_number', 'student_number')}),)
+    fieldsets = UserAdmin.fieldsets + (
+        ('ILES', {'fields': ('role', 'department', 'staff_number', 'student_number')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('ILES', {'fields': ('role', 'department', 'staff_number', 'student_number')}),
+    )
+
 
 @admin.register(InternshipPlacement)
 class PlacementAdmin(admin.ModelAdmin):
     list_display = ('student', 'company_name', 'start_date', 'end_date', 'is_active')
     list_filter = ('is_active',)
 
+
+@admin.register(WeeklyLog)
+class WeeklyLogAdmin(admin.ModelAdmin):
+    list_display = ('student', 'week_number', 'status', 'submitted_at')
+    list_filter = ('status',)
+
+
 admin.site.register(EvaluationCriteria)
 admin.site.register(CriteriaScore)
+
 
 @admin.register(Evaluation)
 class EvaluationAdmin(admin.ModelAdmin):
