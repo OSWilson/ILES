0fe09d2f05f1a4ed1fdc509ddfe01a89293f1f31
OSWilson <stewartoluka@gmail.com>
2026-06-12 14:08:35 +0300

    Auto create superuser on deploy

--- backend/build.sh
+++ backend/build.sh
@@ -9,3 +9,11 @@ python manage.py collectstatic --no-input
 
 
 python manage.py migrate
+
+
+
+
+python manage.py createsuperuser \
+  --noinput \
+  --username superadmin \
+  --email superadmin@iles.ac.ug || true
